c5b06011ccf879bc36af68c69282d1a2215d22f7
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-07-23 22:28:20 -0500

    Setup dev environment

--- server/server.js
+++ server/server.js
@@ -3,21 +3,28 @@ const app = express();              // create app instance
 const db = require('./database');   // database.js connection
 
 const cors = require('cors');
+
 const corsOptions = {
     // accept requests from these URLs only
-    origin: [process.env.CLIENT_URL,
-        process.env.CLIENT_URL_SECOND,
-    ],
+
+    // origin: [process.env.CLIENT_URL,
+    //     process.env.CLIENT_URL_SECOND,
+    // ],
+
+    origin: ["http://localhost:5173"],
 };
+
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 // displayDiv.jsx /weather/recent
 app.get("/weather/recent", async (req, res) => {
     try {
         const result = await db.query('SELECT * FROM weather ORDER BY "currTimestamp" DESC LIMIT 1');
         console.log("Most recent snapshot fetched.");
+
         res.json({
             timestamp: result.rows[0].currTimestamp,
             temp_fahrenheit: result.rows[0].tempFahrenheit,
@@ -33,20 +40,13 @@ app.get("/weather/recent", async (req, res) => {
 app.get("/weather/past", async (req, res) => {
     try {
         const result = await db.query('SELECT * FROM weather ORDER BY "currTimestamp" DESC LIMIT 5');
+        console.log("Past snapshots fetched.");
 
         const resultRows = result.rows.slice().reverse().map(row => ({
             timestamp: row.currTimestamp,
             temp_fahrenheit: row.tempFahrenheit,
             humidity: row.humidityPercentage,
         }));
-
-        // const resultRows = [
-        //     { timestamp: result.rows[4].currTimestamp, temp_fahrenheit: result.rows[4].tempFahrenheit, humidity: result.rows[4].humidityPercentage },
-        //     { timestamp: result.rows[3].currTimestamp, temp_fahrenheit: result.rows[3].tempFahrenheit, humidity: result.rows[3].humidityPercentage },
-        //     { timestamp: result.rows[2].currTimestamp, temp_fahrenheit: result.rows[2].tempFahrenheit, humidity: result.rows[2].humidityPercentage },
-        //     { timestamp: result.rows[1].currTimestamp, temp_fahrenheit: result.rows[1].tempFahrenheit, humidity: result.rows[1].humidityPercentage },
-        //     { timestamp: result.rows[0].currTimestamp, temp_fahrenheit: result.rows[0].tempFahrenheit, humidity: result.rows[0].humidityPercentage },
-        // ];
 
         res.json(resultRows);
     } catch (error) {
@@ -56,14 +56,14 @@ app.get("/weather/past", async (req, res) => {
 })
 
 // little-paw /weather
-// app.post("/weather", async (req, res) => {
-//     try {
-//         console.log("LITTLE-PAW: ", req);
-//     } catch (error) {
-//         console.error("ERROR: unable to post current temperature + humidity.");
-//         res.status(500).json({ error: "Failed to post data." });
-//     }
-// })
+app.post("/weather/update", async (req, res) => {
+    try {
+        // TODO: POST DATA TO DATABASE
+    } catch (error) {
+        console.error("ERROR: unable to post current temperature + humidity.");
+        res.status(500).json({ error: "Failed to post data." });
+    }
+})
 
 // run app
 app.listen(PORT, () => {
